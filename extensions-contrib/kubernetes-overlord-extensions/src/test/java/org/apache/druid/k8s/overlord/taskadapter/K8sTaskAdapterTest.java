55f9e03d9ea5493ba190d80da4389054acc24371
Sensor <fectrain@outlook.com>
2024-02-23 18:14:23 +0800

    merge minor conflicts

--- extensions-contrib/kubernetes-overlord-extensions/src/test/java/org/apache/druid/k8s/overlord/taskadapter/K8sTaskAdapterTest.java
+++ extensions-contrib/kubernetes-overlord-extensions/src/test/java/org/apache/druid/k8s/overlord/taskadapter/K8sTaskAdapterTest.java
@@ -409,10 +409,9 @@ class K8sTaskAdapterTest
         taskConfig,
         startupLoggingConfig,
         node,
-        jsonMapper,
-        taskLogs
+        jsonMapper
     );
-    NoopTask task = K8sTestUtils.createTask("id", 1);
+    NoopTask task = NoopTask.create("id", 1);
     Job actual = adapter.createJobFromPodSpec(
         pod.getSpec(),
         task,
